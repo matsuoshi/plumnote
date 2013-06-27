9159a0cd7002b25f48b30d06862368df9f7f38eb
matsuo <matsuo@n-e-u.co.jp>
2013-06-27 18:02:42 +0900

    first commit

---
+++ plumnote.js
@@ -0,0 +1,28 @@
+/*
+ *	plumnote.js
+ */
+
+jQuery(function($)
+{
+	// tweet button
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+	
+	// pretty-print
+	$('article pre').addClass('prettyprint').addClass('linenums');
+	prettyPrint();
+});
+
+
+var plumnote = {
+	recent_posts: function(resp) {
+		var posts = resp.posts;
+		jQuery('#sidebar .sidebar-recent > div:first').replaceWith('<ul/>');
+		var $ul = jQuery('#sidebar .sidebar-recent ul');
+		for (var i = 0; i < posts.length; i++) {
+			var p = posts[i];
+			var type = p.type;
+			var title = p[type + '-title'] || p[type + '-text'] || p['slug'] || 'No title';
+			$ul.append('<li><a href="'+p['url']+'">'+title+'</a></li>');
+		}
+	}
+};
