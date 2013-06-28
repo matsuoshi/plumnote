4c503f6303cc8e2893148ec0cc6b699a5252e4e2
matsuo <matsuo@n-e-u.co.jp>
2013-06-28 16:53:13 +0900

    IE と Firefox での表示崩れを修正

--- plumnote.js
+++ plumnote.js
@@ -13,16 +13,14 @@ jQuery(function($)
 });
 
 
-var plumnote = {
-	recent_posts: function(resp) {
-		var posts = resp.posts;
-		jQuery('#sidebar .sidebar-recent > div:first').replaceWith('<ul/>');
-		var $ul = jQuery('#sidebar .sidebar-recent ul');
-		for (var i = 0; i < posts.length; i++) {
-			var p = posts[i];
-			var type = p.type;
-			var title = p[type + '-title'] || p[type + '-text'] || p['slug'] || 'No title';
-			$ul.append('<li><a href="'+p['url']+'">'+title+'</a></li>');
-		}
+function plumnote_recent_posts(resp) {
+	var posts = resp.posts;
+	jQuery('#sidebar .sidebar-recent > div:first').replaceWith('<ul/>');
+	var $ul = jQuery('#sidebar .sidebar-recent ul');
+	for (var i = 0; i < posts.length; i++) {
+		var p = posts[i];
+		var type = p.type;
+		var title = p[type + '-title'] || p[type + '-text'] || p['slug'] || 'No title';
+		$ul.append('<li><a href="'+p['url']+'">'+title+'</a></li>');
 	}
 };
